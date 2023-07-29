44eb623a31d01bd7235bc7b2db3d695ea5ced622
Freddi <mail@FrederikHeinrich.de>
2023-07-30 01:20:26 +0200

    Update MongoConnection.java

--- src/main/java/io/freddi/personalizedfavicon/utils/MongoConnection.java
+++ src/main/java/io/freddi/personalizedfavicon/utils/MongoConnection.java
@@ -1,5 +1,6 @@
 package io.freddi.personalizedfavicon.utils;
 
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
@@ -7,22 +8,27 @@ import dev.morphia.query.filters.Filters;
 import io.freddi.personalizedfavicon.entities.PersonalizedFavicon;
 import lombok.Getter;
 
+import java.util.ArrayList;
+
 @Getter
 public class MongoConnection {
 
     public static MongoConnection instance;
-    private final Datastore datastore;
+    private MongoClient client;
+    private Datastore datastore;
 
-    public MongoConnection(String uri, String databaseName) {
+    public MongoConnection(String databaseName) {
         instance = this;
-        datastore = Morphia.createDatastore(MongoClients.create(uri), databaseName);
-        datastore.getMapper().mapPackage("io.freddi.personalizedfavicon.entities");
+        client = MongoClients.create(Secrets.instance.getMongodb());
+        datastore = Morphia.createDatastore(client, databaseName);
+        datastore.getMapper().map(PersonalizedFavicon.class);
         datastore.ensureIndexes();
     }
 
-    public PersonalizedFavicon getFavicon(String ip) {
-        return datastore.find(PersonalizedFavicon.class).filter(Filters.eq("ip", ip)).first();
+    public void close(){
+        client.close();
+        instance = null;
+        datastore = null;
     }
-
 
 }
