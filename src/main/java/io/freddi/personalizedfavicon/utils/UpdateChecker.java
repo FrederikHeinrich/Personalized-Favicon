f86af52a36cc98c3c64af6c0b29f5aef64506466
Freddi <mail@FrederikHeinrich.de>
2023-07-30 01:20:20 +0200

    Create UpdateChecker.java

---
+++ src/main/java/io/freddi/personalizedfavicon/utils/UpdateChecker.java
@@ -0,0 +1,7 @@
+package io.freddi.personalizedfavicon.utils;
+
+public class UpdateChecker {
+
+    private static final String VERSION_URL = "https://raw.githubusercontent.com/FreddiDev/PersonalizedFavicon/master/version.txt";
+
+}
