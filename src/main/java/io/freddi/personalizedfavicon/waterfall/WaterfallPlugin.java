1f3a6dae4c212d6940b390216cd75924f6de9729
Freddi <mail@FrederikHeinrich.de>
2023-07-30 01:20:08 +0200

    Update WaterfallPlugin.java

--- src/main/java/io/freddi/personalizedfavicon/waterfall/WaterfallPlugin.java
+++ src/main/java/io/freddi/personalizedfavicon/waterfall/WaterfallPlugin.java
@@ -2,9 +2,14 @@ package io.freddi.personalizedfavicon.waterfall;
 
 import io.freddi.personalizedfavicon.entities.PersonalizedFavicon;
 import io.freddi.personalizedfavicon.utils.Config;
+import io.freddi.personalizedfavicon.utils.Messages;
+import lombok.Getter;
+import lombok.NonNull;
+import net.kyori.adventure.platform.bungeecord.BungeeAudiences;
 import net.md_5.bungee.api.Favicon;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.Server;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -13,13 +18,21 @@ import net.md_5.bungee.event.EventHandler;
 
 public class WaterfallPlugin extends Plugin implements Listener {
 
+    @Getter
+    private static BungeeAudiences adventure;
     @Override
     public void onEnable() {
+
+        adventure = BungeeAudiences.create(this);
         getLogger().info("Starting PersonalizedFavicon for Waterfall");
         Metrics metrics = new Metrics(this, 5495);
         metrics.addCustomChart(new Metrics.SingleLineChart("stored_favicons", PersonalizedFavicon::count));
         //
-        assert Config.instance() != null;
+        Config.instance().reload();
+        Messages.instance().reload();
+
+        getProxy().getPluginManager().registerCommand(this, new WaterfallCommand());
+        getProxy().getPluginManager().registerListener(this, this);
     }
 
     @EventHandler
@@ -34,10 +47,25 @@ public class WaterfallPlugin extends Plugin implements Listener {
     }
 
     @EventHandler
-    public void onLogin(ServerConnectedEvent event) {
-        String uuid = event.getPlayer().getUniqueId().toString();
-        String ip = event.getPlayer().getPendingConnection().getVirtualHost().getHostString();
-        String username = event.getPlayer().getName();
+    public void onLogin(LoginEvent event) {
+        String uuid = event.getConnection().getUniqueId().toString();
+        String ip = event.getConnection().getVirtualHost().getHostString();
+        String username = event.getConnection().getName();
         PersonalizedFavicon favicon = PersonalizedFavicon.find(uuid, username, ip);
     }
+
+    @Override
+    public void onDisable() {
+        if(this.adventure != null) {
+            this.adventure.close();
+            this.adventure = null;
+        }
+    }
+    public @NonNull BungeeAudiences adventure() {
+        if(this.adventure == null) {
+            throw new IllegalStateException("Cannot retrieve audience provider while plugin is not enabled");
+        }
+        return this.adventure;
+    }
+
 }
