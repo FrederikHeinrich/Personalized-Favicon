40ce8788be00d75a332e1270bda540a733c2799d
Freddi <mail@FrederikHeinrich.de>
2023-07-30 20:40:24 +0200

    Update VelocityPlugin.java

--- src/main/java/io/freddi/personalizedfavicon/velocity/VelocityPlugin.java
+++ src/main/java/io/freddi/personalizedfavicon/velocity/VelocityPlugin.java
@@ -1,31 +1,36 @@
 package io.freddi.personalizedfavicon.velocity;
 
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.util.Favicon;
 import io.freddi.personalizedfavicon.entities.PersonalizedFavicon;
 import io.freddi.personalizedfavicon.utils.Config;
 import io.freddi.personalizedfavicon.utils.Messages;
+import io.freddi.personalizedfavicon.utils.UpdateChecker;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
 import java.nio.file.Path;
 
-@Plugin(id = "personalizedfavicon", name = "PersonalizedFavicon", version = "@version@", description = "PersonalizedFavicon plugin for Velocity", authors = {"Freddi"})
+@Plugin(id = "personalizedfavicon", name = "PersonalizedFavicon", version = "@version@", description = "PersonalizedFavicon plugin for Velocity", authors = {"Freddi"}, url = "https://discord.gg/freddi")
 public class VelocityPlugin {
 
     private final Metrics.Factory metricsFactory;
     private final ProxyServer proxyServer;
+
     @Inject
     public VelocityPlugin(ProxyServer proxyServer, Logger logger, @DataDirectory
     Path dataDirector, Metrics.Factory metricsFactory) {
         this.proxyServer = proxyServer;
-        logger.info("Starting PersonalizedFavicon for Velocity"); this.metricsFactory = metricsFactory;
+        logger.info("Starting PersonalizedFavicon for Velocity");
+        this.metricsFactory = metricsFactory;
         Config.instance().reload();
         Messages.instance().reload();
     }
@@ -54,7 +59,18 @@ public class VelocityPlugin {
         String ip = event.getPlayer().getRemoteAddress().getAddress().getHostAddress();
         String username = event.getPlayer().getUsername();
         PersonalizedFavicon favicon = PersonalizedFavicon.find(uuid, username, ip);
+    }
 
+
+    @Subscribe
+    public void onConnected(ServerConnectedEvent event) {
+        Player player = event.getPlayer();
+        if (Config.instance().updateChecker().notification().user())
+            if (player.hasPermission("personalizedfavicon.updates")) {
+                if (UpdateChecker.instance.available()) {
+                    player.sendMessage(UpdateChecker.instance.updateAvailable());
+                }
+            }
     }
 
 }
